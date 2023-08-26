e311796a37d07cf96554a2588833f3f4f61461bf
shrirambansal <76605834+shrirambansal@users.noreply.github.com>
2023-08-26 19:41:08 +0530

    Create lesseror greater

---
+++ loop_3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b ;
+    cin >> a >> b;
+    if(a<b) {
+        cout<<"a is lesser than b";
+    }
+    else {
+        cout<<"b is lesser than a";
+    }
+
+}

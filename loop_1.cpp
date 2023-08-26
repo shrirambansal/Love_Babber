dc892ff4ace1da580df99393812903fe07760bc3
shrirambansal <76605834+shrirambansal@users.noreply.github.com>
2023-08-26 19:39:16 +0530

    Create check the number

---
+++ loop_1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a = 10;
+    if (a<10)
+    {
+        cout<<"a is greater than";
+    }
+    else {
+        cout<<"less than";
+    }
+    
+
+     
+     
+}

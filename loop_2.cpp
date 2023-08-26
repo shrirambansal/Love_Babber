ad7675d8779a2578e5da38a0bd8cd78ac8460b51
shrirambansal <76605834+shrirambansal@users.noreply.github.com>
2023-08-26 19:40:19 +0530

    Create negativeor positive

---
+++ loop_2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    cout<<"Hello";
+    if(n>0) {
+        cout<<"Number is positive";
+    }
+    else {
+        cout<<"number is negetive";
+    }
+
+}

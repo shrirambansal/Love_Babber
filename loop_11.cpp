08030dfe60f91551e255e67173a1b2b56a4cd411
shrirambansal <76605834+shrirambansal@users.noreply.github.com>
2023-09-01 16:33:02 +0530

    Create hi hello reply me

---
+++ loop_11.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    for (int i = 1; i <= 5; i++) {
+        cout << "hello\n";
+        cout << "hi\n";
+        continue;
+        cout << "reply me\n";
+    }
+}

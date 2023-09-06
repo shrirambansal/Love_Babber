9c4635415e5a95c14dd6e22a8c0afe079fd5b796
shrirambansal <76605834+shrirambansal@users.noreply.github.com>
2023-09-06 10:35:23 +0530

    Create pattern_17.cpp

---
+++ pattern_17.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    int i=1;
+    char ch ='A';
+    while (i <= n)
+    {
+        int j = 1;
+        while (j <= i)    
+            {
+                char ch = 'A'+i+j-2;
+                cout << ch <<" ";
+                j = j+1;
+            }
+        cout << endl;
+        i = i+1;
+    }
+}

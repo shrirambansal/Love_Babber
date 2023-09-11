fb006c9bf42e29a03c1530d1125151e78ae0edf7
shrirambansal <76605834+shrirambansal@users.noreply.github.com>
2023-09-11 20:18:30 +0530

    Create find the Area and Perimeter of a Rectangle.

---
+++ Practice_9.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+    int main()
+    {
+    	int width, lngth, area, peri;
+		cout << "\n\n Find the Area and Perimeter of a Rectangle :\n";
+		cout << "-------------------------------------------------\n";		
+        cout<<" Input the length of the rectangle : ";
+    	cin>>lngth;
+		cout<<" Input the width of the rectangle : ";
+    	cin>>width;
+    	area=(lngth*width);
+		peri=2*(lngth+width);
+        cout<<" The area of the rectangle is : "<< area << endl;
+        cout<<" The perimeter of the rectangle is : "<< peri << endl;		
+        cout << endl;
+        return 0;
+    }

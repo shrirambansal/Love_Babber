a94c91652406f52ff214b5bdae239e236340b4df
shrirambansal <76605834+shrirambansal@users.noreply.github.com>
2023-09-12 21:41:56 +0530

    Create converts kilometers per hour to miles per hour.

---
+++ Practice_14.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float kmph, miph;
+	cout << "\n\n Convert kilometers per hour to miles per hour :\n";
+	cout << "----------------------------------------------------\n";	
+    cout << " Input the distance in kilometer : ";
+    cin >> kmph;
+	miph = (kmph * 0.6213712); 
+    cout << " The "<< kmph <<" Km./hr. means "<< miph << " Miles/hr." << endl;
+	cout << endl;
+    return 0;
+}	

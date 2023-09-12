131f1cf2cfb0ed48a5e2ad9ea8ebdaa59166e774
shrirambansal <76605834+shrirambansal@users.noreply.github.com>
2023-09-12 21:43:35 +0530

    Create  convert temperature in Kelvin to Fahrenheit.

---
+++ Practice_15.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    float kel, frh;
+		cout << "\n\n Convert temperature in Kelvin to Fahrenheit  :\n";
+		cout << "---------------------------------------------------\n";	
+    cout << " Input the temperature in Kelvin : ";
+    cin >> kel;
+    frh = (9.0 / 5) * (kel - 273.15) + 32;
+    cout << " The temperature in Kelvin    : " << kel << endl;
+    cout << " The temperature in Fahrenheit : " << frh << endl;
+	cout << endl;
+    return 0;
+}

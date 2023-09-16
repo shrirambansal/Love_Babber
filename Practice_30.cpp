9d6abb2fc581c4ebecf85fcf8d6ffc9bf27cdd81
shrirambansal <76605834+shrirambansal@users.noreply.github.com>
2023-09-16 08:02:33 +0530

    Create  print the following pattern.

---
+++ Practice_30.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    cout << "\n\n Print the following pattern:\n";
+	cout << "--------------------------------\n";
+	cout << " xxxxx\n";
+	cout << "x     x       x        x\n";
+	cout << "x             x        x\n";
+	cout << "x          xxxxxxx  xxxxxxx\n";
+	cout << "x             x        x\n";
+	cout << "x     x       x        x\n";
+	cout << " xxxxx\n";
+}

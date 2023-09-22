b25818b7e95182d2c5fa380f1012bf2dc34697a5
shrirambansal <76605834+shrirambansal@users.noreply.github.com>
2023-09-22 22:56:13 +0530

    that reads seven numbers and sorts them in descending order

---
+++ Practice_56.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num[7];
+    for (int i = 0; i != 7; ++i) {
+        cin >> num[i];
+    }
+    sort(num, num+7);
+    cout << " " << num[6]  << " " << num[5]  << " " << num[4]  << " " << num[3]  << " " << num[2] << " " << num[1] << " " << num[0];
+    return 0;
+}
